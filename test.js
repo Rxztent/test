eb9784a888eeb78d9c0d007baf59bd6d6708dc1b
Rxztent <49635832+Rxztent@users.noreply.github.com>
2023-11-17 12:28:37 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,37 @@
+window.testMod = {};
+
+////////////////////////////////////////////////////////////////////
+// RUNCODEBEFORE
+////////////////////////////////////////////////////////////////////
+
+window.testMod.runCodeBefore = function() {
+  // Add an event listener for the 'E' key press
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'e' || e.key === 'E') {
+      // Spawn an apple
+      window.snakeGame.pixelList.push({
+        x: Math.floor(window.snakeGame.currentBoardWidth * 3 / 4),
+        y: Math.floor(window.snakeGame.currentBoardHeight / 2),
+        category: 'apple',
+        type: 0
+      });
+    }
+  });
+}
+
+////////////////////////////////////////////////////////////////////
+// ALTERSNAKECODE
+////////////////////////////////////////////////////////////////////
+
+window.testMod.alterSnakeCode = function(code) {
+  // You can modify the game code here if needed
+  return code;
+}
+
+////////////////////////////////////////////////////////////////////
+// RUNCODEAFTER
+////////////////////////////////////////////////////////////////////
+
+window.testMod.runCodeAfter = function() {
+  // Add any code that should run after the game code
+}
